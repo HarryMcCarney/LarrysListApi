b35eb5e241bd557b83c15077e0dee370f6aa70bb
Harry <hmccarney@gmail.com>
2013-01-22 17:41:17 +0100

    fixed deploy bug

--- DeployLarrysListDev.ps1
+++ DeployLarrysListDev.ps1
@@ -13,7 +13,7 @@ Remove-item c:\inetpub\wwwroot\LarrysList\LarrysList_Dev\LarrysListApi -recurse
 New-Item c:\inetpub\wwwroot\LarrysList\LarrysList_Dev\LarrysListApi -type directory
 copy-item c:\Hudson\jobs\LarrysList_Dev\workspace\LarrysList\LarrysList\bin c:\inetpub\wwwroot\LarrysList\LarrysList_Dev\LarrysListApi -recurse
 copy-item c:\Hudson\jobs\LarrysList_Dev\workspace\LarrysList\LarrysList\web.config c:\inetpub\wwwroot\LarrysList\LarrysList_Dev\LarrysListApi
-copy-item c:\Hudson\jobs\LarrysList_Dev\workspace\LarrysList\LarrysList\global.asax c:\inetpub\wwwroot\BurnPlus\LarrysList_Dev\LarrysListApi
+copy-item c:\Hudson\jobs\LarrysList_Dev\workspace\LarrysList\LarrysList\global.asax c:\inetpub\wwwroot\LarrysList\LarrysList_Dev\LarrysListApi
 copy-item c:\Hudson\jobs\LarrysList_Dev\workspace\LarrysList\LarrysList\nlog.config c:\inetpub\wwwroot\LarrysList\LarrysList_Dev\LarrysListApi
 
 #start sites and services 
