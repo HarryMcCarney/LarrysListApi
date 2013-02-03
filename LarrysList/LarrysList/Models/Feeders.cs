891f70ef5d1b9dc88b42ec14ae259fd479e16d50
Harry <hmccarney@gmail.com>
2013-02-03 16:25:27 +0100

    added feeders list

--- LarrysList/LarrysList/Models/Feeders.cs
+++ LarrysList/LarrysList/Models/Feeders.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 
 namespace LarrysList.Models
 {
     [Serializable]
     public class Feeders
     {
+        [XmlElement]
         public List<Feeder> Feeder { get; set; }
     }
 }
